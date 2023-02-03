befb828d15272fddc748a7d0c3d28b790e8a113d
mario <sks_894@hotmail.com>
2023-02-03 09:09:20 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.css';
+import React from 'react';
+import Slideshow from './components/Slideshow';
+import styled from 'styled-components';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <Title>Productos destacados</Title>
+      <Slideshow />
+    </main>
   );
 }
 
-export default App;
+const Title = styled.h2`
+  font-size: 18px;
+  font-weight: 700;
+  text-transform: uppercase;
+  margin-bottom: 10px;
+`;
---
+++ src/components/Slideshow.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Slideshow() {
+  return (
+    <div>
+      <div>
+        <a href='https://www.falconmasters.com'>Hello</a>
+      </div>
+      <div>Controles</div>
+    </div>
+  );
+}
