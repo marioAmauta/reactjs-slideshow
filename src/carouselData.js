6b6d9c204cbe8fac1fa5bc1931b20475eef17021
Mario <sks_894@hotmail.com>
2023-02-07 00:20:10 -0300

    separate carousel data from app component

---
+++ src/carouselData.js
@@ -0,0 +1,25 @@
+import img1 from './img/1.jpg';
+import img2 from './img/2.jpg';
+import img3 from './img/3.jpg';
+import img4 from './img/4.jpg';
+
+const slideData = [
+  {
+    text: '15% discount on Apple products',
+    image: img1
+  },
+  {
+    text: '15% discount on Apple products',
+    image: img2
+  },
+  {
+    text: '15% discount on Apple products',
+    image: img3
+  },
+  {
+    text: '15% discount on Apple products',
+    image: img4
+  }
+];
+
+export { slideData };
